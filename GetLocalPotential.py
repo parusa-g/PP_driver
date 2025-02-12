cec036a995df2a04473d0f793c962834ae4100b1
parusa-g <gian.parusa@psi.ch>
2025-02-12 10:46:22 +0100

    stream-lining the workflow

--- GetLocalPotential.py
+++ GetLocalPotential.py
@@ -1,7 +1,6 @@
 import os 
 import numpy as np
 import f90nml
-import subprocess
 #===============================================================================
 class ExtractPotential(object):
    def __init__(self,mpicmd,exe_path,prefix,outdir,plot_num,iflag,latvec_file,atoms_file,inp_prefix,out_prefix,pot_prefix):
@@ -125,16 +124,16 @@ class ExtractPotential(object):
 
       return nearest_neighbor, nnvec
 #-------------------------------------------------------------------------------   
-   def WriteRunScript(self,file_in,file_out,atomref_id='',initialized=True):
+   def WriteRunScript(self,atomref_ids):
       
-      if not initialized:
-         run_script = f'run_init.sh'
-      else:
-         run_script = f'run_atom#{atomref_id}.sh'
+      run_script = 'run_pp.sh'
+
+      FileInp_init = self.inp_prefix + self.prefix + '.pp.inp'
+      FileOut_init = self.out_prefix + self.prefix + '.pp.out'
 
       with open(run_script,'w') as f:
          f.write('#!/bin/bash\n')
-         f.write(f'#SBATCH --job-name=atom_{atomref_id}\n')
+         f.write(f'#SBATCH --job-name=pp.x\n')
          f.write('#SBATCH --cluster=merlin6\n')
          f.write('#SBATCH --partition=hourly\n')
          f.write('#SBATCH --time=00:05:00\n')
@@ -144,22 +143,40 @@ class ExtractPotential(object):
          f.write('#SBATCH --ntasks=6\n')
          f.write('#SBATCH --ntasks-per-core=1\n')
          f.write('#SBATCH --nodes=1\n')
+         
+         atomref_ids_str = ','.join([str(i) for i in atomref_ids])
+         f.write(f'#SBATCH --array=[{atomref_ids_str}]\n')
          f.write('\n')
-         f.write(f'{self.mpicmd} {self.exe} < {file_in} > {file_out}\n')
+         
+         f.write(f'PP={self.exe}\n')
+         f.write('\n')
+         
+         f.write(f'if [ ! -f {FileOut_init} ]; then\n')
+         f.write(f'  {self.mpicmd} $PP < {FileInp_init} > {FileOut_init}\n')
+         f.write(f'fi\n')
+         f.write('\n')
+         f.write(f'while ! grep -q "JOB DONE" {FileOut_init}; do\n')
+         f.write(f'  sleep 1\n')
+         f.write('done\n')
+         f.write('\n')
+         
+         label = 'Atom\#${SLURM_ARRAY_TASK_ID}'
+         file_in = self.inp_prefix + self.prefix + '_' + label + '.pp.inp'
+         file_out = self.out_prefix + self.prefix + '_' + label + '.pp.out'
+         
+         f.write(f'{self.mpicmd} $PP < {file_in} > {file_out}\n')
+         f.write('\n')
 
 #-------------------------------------------------------------------------------
    def WriteInputs(self,atomref_ids):
-      file_inp = self.inp_prefix + prefix + '.pp.inp'
-      file_out = self.out_prefix + prefix + '.pp.out'
-
-      self.WriteInputPPfile(file_inp, initialized=False)
-      self.WriteRunScript(file_inp,file_out,initialized=False)
+      
+      FileInp_init = self.inp_prefix + self.prefix + '.pp.inp'
+      self.WriteInputPPfile(FileInp_init, initialized=False)
       
       for i in range(len(atomref_ids)):
          atomref_id = atomref_ids[i]
-         
+
          atom_label = 'Atom#' + str(atomref_id)
-         atom_label += '_' + self.atom_labels[atomref_id]
 
          nn_id, nnvec = self.FindNearestNeighborVector(atomref_id)
 
@@ -167,36 +184,19 @@ class ExtractPotential(object):
          print(f'Nearest neighbor atom: {self.atom_labels[nn_id]}, # {nn_id}')
          print(f'Nearest neighbor vector: {nnvec[0]:.6f}, {nnvec[1]:.6f}, {nnvec[2]:.6f}')
 
-         file_inp = self.inp_prefix + self.prefix + '_' + atom_label + '.pp.inp'
-         file_out = self.out_prefix + self.prefix + '_' + atom_label + '.pp.out'
-
          vec = 0.5 * nnvec
+         
+         file_inp = self.inp_prefix + self.prefix + '_' + atom_label + '.pp.inp'
 
-         self.WriteInputPPfile(file_inp,atom_label,self.atompos_cart[atomref_id,:],vec)
-         self.WriteRunScript(file_inp,file_out,atomref_id)
+         self.WriteInputPPfile(file_inp, atom_label, self.atompos_cart[atomref_id,:], vec)
+         
 #-------------------------------------------------------------------------------
    def run(self,atomref_ids):
       self.WriteInputs(atomref_ids)
+      self.WriteRunScript(atomref_ids)
       
-      init_is_done = False
+      os.system('sbatch run_pp.sh')
       
-      init_out = self.out_prefix + self.prefix + '.pp.out'
-      if not os.path.exists(init_out):
-         print('Running the initialization calculation')
-         os.system('sbatch run_init.sh') 
-      else:
-         print('Initialization calculation is already done')
-      
-      while not init_is_done:
-         try:
-            if os.path.exists(init_out):
-               output = subprocess.check_output(f'grep "JOB DONE" {init_out}', shell=True)
-               init_is_done = True
-         except:
-            init_is_done = False
-      
-      for i in range(len(atomref_ids)):
-         os.system(f'sbatch run_atom#{atomref_ids[i]}.sh')
 #-------------------------------------------------------------------------------       
 #===============================================================================
 if __name__ == '__main__':
